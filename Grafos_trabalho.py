4ece0a87b556cd260aea8a85fdc4cf6275ef8178
Gustavo Henrique Franzin da Silva <111886757+GustavoHFranzin@users.noreply.github.com>
2023-01-16 13:34:21 -0300

    Update Grafos_trabalho.py

--- Grafos_trabalho.py
+++ Grafos_trabalho.py
@@ -5,10 +5,11 @@ class grafo:
             dicionario_grafo = {}
         self.dicionario_grafo = dicionario_grafo
 
-    def get_vertices(self):#Mostra todas os vertices
+
+    def get_vertices(self):#Mostra todas os vertices NÃO orientado
         return list(self.dicionario_grafo.keys())
 
-    def find_edges(self):#Printa todas as arestas
+    def find_edges(self):#Printa todas as arestas NÃO orientado
         edge_name = []
         for vertex in self.dicionario_grafo:
             for next_vertex in self.dicionario_grafo[vertex]:
@@ -17,11 +18,14 @@ class grafo:
 
         return edge_name
 
-    def add_vertice(self, vertice):
+    def add_vertice(self, vertice):#NÃO orientado
         if vertice not in self.dicionario_grafo:
             self.dicionario_grafo[vertice] = []
 
-    def add_aresta(self, aresta):
+    def remove_vertice(self, vertice):#Remove vertice NÃO ORIENTADO
+        del self.dicionario_grafo[vertice]
+
+    def add_aresta(self, aresta):#NÃO orientado
         aresta = set(aresta)
         (vertex_1, vertex_2) = tuple(aresta)
         if vertex_1 in self.dicionario_grafo:
@@ -29,6 +33,24 @@ class grafo:
         else:
             self.dicionario_grafo[vertex_1] = [vertex_2]
 
+
+
+    def print_grafo_lista(self):#printa a lista de adjacencia para grafos NÃO orientados
+        for item in self.dicionario_grafo.items():
+            print(item)
+
+    def print_grafo_matriz(self):#Printa a matrix de adjancia para grafos NÃO orientados (falta colocar um end line a cada linha)
+        keys = sorted(graph.keys())
+        size = len(keys)
+        matrix = [[0] * size for i in range(size)]
+        # We iterate over the key:value entries in the dictionary first,
+        # then we iterate over the elements within the value
+        for a, b in [(keys.index(a), keys.index(b))
+            for a, row in graph.items() for b in row]:
+            # Use 1 to represent if there's an edge
+            # Use 2 to represent when node meets itself in the matrix (A -> A)
+            matrix[a][b] = 2 if (a == b) else 1
+        print(matrix)
 
 # Create the dictionary with graph elements
 graph = {
@@ -39,12 +61,24 @@ graph = {
             'e': []
     }
 
+grafo_direcionado = {
+            'a': [],
+            'b': ['a', 'e'],
+            'c': ['a', 'b', 'd'],
+            'd': ['b'],
+            'e': ['b', 'd']
+    }
 
 g = grafo(graph)
+d = grafo(grafo_direcionado)
 
-print(g.get_vertices())
-g.add_vertice("f")
-print(g.get_vertices())
+#print(g.get_vertices())
+print(d.get_vertices())
+#print(g.print_grafo_lista())
+print(d.remove_vertice('b'))
+print(d.get_vertices())
+d.add_vertice('f')
+print(d.get_vertices())
 
 
 # def le_txt(self, dados):
